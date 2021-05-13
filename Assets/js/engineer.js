031686ebb0b47844413bfb95b8140d70f158b726
Statikman619 <martinez.michael02@gmail.com>
2021-05-13 11:21:03 -0700

    added engineer js

---
+++ Assets/js/engineer.js
@@ -0,0 +1,30 @@
+inquirer = require("inquirer");
+
+const engineerQ = [
+  {
+    type: "input",
+    name: "engineerName",
+    message: "What is your engineer's name?",
+  },
+  {
+    type: "input",
+    name: "engineerID",
+    message: "What is your engineer's id number?",
+  },
+  {
+    type: "input",
+    name: "engineerEmail",
+    message: "What is your engineer's email?",
+  },
+  {
+    type: "input",
+    name: "engineerGit",
+    message: "What is your engineer's GitHub username?",
+  },
+  {
+    type: "list",
+    name: "addEmp",
+    message: "Add another team member?",
+    choices: ["Engineer", "Intern", "No more members"],
+  },
+];
