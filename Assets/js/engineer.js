1cacc9e9d197d8389e3fdc0c2d0f829a4b387fec
Statikman619 <martinez.michael02@gmail.com>
2021-05-13 11:21:43 -0700

    more  engineer js

--- Assets/js/engineer.js
+++ Assets/js/engineer.js
@@ -28,3 +28,23 @@ const engineerQ = [
     choices: ["Engineer", "Intern", "No more members"],
   },
 ];
+function eng() {
+  return inquirer.prompt(engineerQ);
+}
+function generate(answer) {
+  return `
+    <div class="card" style="width: 18rem;">
+    <img src="assets/2933820.svg" class="card-img-top" alt="engineer_image"> <p class="card-text">Engineer</p>
+    <h5 class="card-title">${answer.engineerName}</h5>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${answer.engineerID}</li>
+    <li class="list-group-item">${answer.engineerEmail}</li>
+    <li class="list-group-item">${answer.engineerGit}</li>
+    </ul>
+    </div>  
+    `;
+}
+module.exports = {
+  eng,
+  generate,
+};
