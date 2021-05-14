7ccf30bc01535640723869a380e6f78ec4e2d8f7
Statikman619 <martinez.michael02@gmail.com>
2021-05-13 17:59:53 -0700

    fixed employee

--- Lib/employee.js
+++ Lib/employee.js
@@ -16,3 +16,22 @@ const employeeQ = [
     name: "engineerEmail",
     message: "What is your engineer's email?",
   },
+];
+function eng() {
+  return inquirer.prompt(employeeQ);
+}
+function generate(answer) {
+  return `
+    <div class="card" style="width: 18rem;">
+    <img src="assets/images/engineer.png" class="card-img-top" alt="engineer_image"> <p class="card-text">Engineer</p>
+    <h5 class="card-title">${answer.engineerName}</h5>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${answer.engineerID}</li>
+    <li class="list-group-item">${answer.engineerEmail}</li>
+    </div>  
+    `;
+}
+module.exports = {
+  eng,
+  generate,
+};
